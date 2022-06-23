213206a6e6b37187465302b15889cb7a0beee7e6
Erik95AL <erik.aguirre.1995.bcn@hotmail.com>
2022-06-23 11:57:38 +0200

    jueves media mañana

---
+++ newproduct.php
@@ -0,0 +1,55 @@
+<?php
+switch ($_POST["newProduct"]) {
+    case 1:
+        peticion1();
+        break;
+    case 2:
+        peticion2();
+        break;
+    case 3:
+        peticion3();
+        break;
+}
+
+
+function peticion1()
+{
+    $conn = new mysqli("localhost", "root", "");
+    if ($conn->connect_error) {
+        die("Connection failed" . $conn->connect_error);
+    }
+    echo "Connected sucessfully";
+    $conn->close();
+}
+
+function peticion2()
+{
+    $conn = new mysqli("localhost", "root", "");
+    if ($conn->connect_error) {
+        die("Connection failed" . $conn->connect_error);
+    }
+    // echo "Connected sucessfully";
+
+    //
+    $conn->close();
+    $sql = "CREATE DATABASE info BMX";
+    if ($conn->query($sql) === TRUE) {
+        echo "Create database \"info BMX\"";
+    } else {
+        echo "Error: create database \"info BMX\" " . $conn->error;
+    }
+
+    //
+    $conn->close();
+}
+
+function peticion3()
+{
+    $conn = new mysqli("localhost", "root", "");
+    $sql = "DROP DATABASE IF EXISTS info BMX";
+    if ($conn->query($sql) === TRUE) {
+        echo "Create database \"info BMX\"";
+    } else {
+        echo "Error: create database \"info BMX\" " . $conn->error;
+    }
+}
