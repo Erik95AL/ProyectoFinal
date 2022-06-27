78ce74f4f0bb52f817c73488c93b1084bcee8b61
Erik95AL <erik.aguirre.1995.bcn@hotmail.com>
2022-06-27 10:16:05 +0200

    lunes-preentrega

--- newproduct.js
+++ newproduct.js
@@ -1,4 +1,13 @@
 function enviarDatos() {
+	console.log(document.getElementById("nameProduct").value);
+	console.log(document.getElementById("costProduct").value);
+	console.log(document.getElementById("addLocation").value);
+	console.log(document.getElementById("sizeColor").value);
+	console.log(document.getElementById("onlyColor").value);
+	console.log(document.getElementById("imageProduct").value);
+	console.log(document.getElementById("textArea").value);
+	console.log(document.getElementById("nameStore").value);
+	console.log(document.getElementById("addLink").value);
 	$.ajax({
 		url: "./newproduct.php",
 		type: "POST",
@@ -6,26 +15,26 @@ function enviarDatos() {
 			api: "crearNuevoproducto",
 			nombre: document.getElementById("nameProduct").value,
 			coste: document.getElementById("costProduct").value,
-			localizacion: document.getElementById("locationLink").value,
+			localizacion: document.getElementById("addLocation").value,
 			tallas: document.getElementById("sizeColor").value,
 			color: document.getElementById("onlyColor").value,
 			imagen: document.getElementById("imageProduct").value,
 			textArea: document.getElementById("textArea").value,
+			nameStore: document.getElementById("nameStore").value,
+			addLink: document.getElementById("addLink").value,
 		},
-		dataType: "json",
+		//dataType: "json",
 		success: function (data) {
-			if (data == 0) {
-				console.log("error");
-			} else {
-				console.log(data);
-				console.log(data.nombre);
-				console.log(data.coste);
-				console.log(data.localizacion);
-				console.log(data.tallas);
-				console.log(data.color);
-				console.log(data.imagen);
-				console.log(data.textArea);
-			}
+			console.log(data);
+			console.log(data.nombre);
+			console.log(data.coste);
+			console.log(data.localizacion);
+			console.log(data.tallas);
+			console.log(data.color);
+			console.log(data.imagen);
+			console.log(data.textArea);
+			console.log(data.nameStore);
+			console.log(data.addLink);
 		},
 		error: function (error) {
 			console.log("ERROR: " + error);
--- newproduct.php
+++ newproduct.php
@@ -1,15 +1,18 @@
 <?php
 switch ($_POST['api']) {
-
     case "crearNuevoproducto":
-        crearProducto($_POST['nombre'], $_POST['coste'], $_POST['localizacion'], $_POST['tallas'], $_POST['color'], $_POST['imagen'], $_POST['textArea']);
+        crearProducto($_POST['nombre'], $_POST['coste'], $_POST['localizacion'], $_POST['tallas'], $_POST['color'], $_POST['imagen'], $_POST['textArea'], $_POST['nameStore'], $_POST['addLink']);
         break;
 }
 
-function crearProducto($nombre, $coste, $localizacion, $tallas, $color, $imagen, $textArea)
+function crearProducto($nombre, $coste, $localizacion, $tallas, $color, $imagen, $textArea, $nameStore, $addLink,)
 {
     $conn = new mysqli('localhost', 'root', '', 'pbd');
-    $sql = "INSERT INTO tablaproducto(nombre,coste,localizacion,tallas,color,imagen,textArea,reg_date) VALUES('" . $nombre . "','" . $coste . "','" . $localizacion . "','" . $tallas . "','" . $color . "','" . $imagen . "','" . $textArea . "')";
+    $sql = "INSERT INTO tablaproducto(nombre,coste,localizacion,tallas,color,imagen,textArea,nameStore,link) VALUES('" . $nombre . "','" . $coste . "','" . $localizacion . "','" . $tallas . "','" . $color . "','" . $imagen . "','" . $textArea . "','" . $nameStore . "','" . $addLink . "')";
+    if ($conn->multi_query($sql) === TRUE) {
+        $last_id = $conn->insert_id;
+    }
+    $conn->close();
 }
 
 //hacer define("dataloquesea",'localhost') fuera sin comillas dobles el dataloquesea.
